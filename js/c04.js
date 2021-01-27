869bbd65da9d0e932d4b0baa9a268004ac1817e1
DianaDouglas <70503002+DianaDouglas@users.noreply.github.com>
2021-01-26 18:32:28 -0800

    Update c04.js

--- js/c04.js
+++ js/c04.js
@@ -1,24 +1,25 @@
-var table = 3; // Unit of table
+var table = 9; // Unit of table
 var operator = 'addition'; // Type of calculation
 var i = 1; // Set counter to 1
 var msg = ''; // Message
-var camvasNames = ['Filler', 'Extra Small', 'Small', 'Medium', 'Normal', 'Large', 'Extra Large', 'Wall Mural'];
+var camvasNames = ['Filler', 'Paperback', 'EBook', 'Audio Book', 'Hard back'];
 
 if (operator === 'addition') {
 	// Do addition
-	while (i < 11) {
+	while (i < 4) {
 		msg += camvasNames[i] + ' Price: ' + '$' + (i+table) + '<br />';
 		i++;
 		table++;
 	}
 } else {
 	// Do multiplication
-	while (i < 11) {
+	while (i < 4) {
 		msg += camvasNames[i] + ' Price: ' + '$' + (i*table) + '<br />';
 		i++;
 		table++;
 	}
 }
+
 
 // Write the message into the page
 var el = document.getElementById('blackboard');
